ea1d5398f116e4d1fff78ab5f7a302cd87880afa
Charlie Sahlman <csahlman@gmail.com>
2013-05-21 10:25:55 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThoughtbotTesting::Application.config.secret_token = 'b909e778e0e0525618d5ece8f11b4fbff8c2c1b5202fcc0e5f0f78a17072280979c4b522232fdcd95b2019cd126f3a1ef1d7d5c220b2524a289466ca9dacca9c'
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = 'gNCQnjcuYgZEgpLxBzRsKg'
+  config.consumer_secret = 'wZHTYL8L3FPznHNBSEY81bWZS1lPeaPZKLr1sqvPPI8'
+  config.oauth_token = '519495227-1nSQEclYvIfsTTqf0EWX6Ymt3qam8sitzzEE9Wsh'
+  config.oauth_token_secret = 'FH9dFw1pCaMlcLOBxE4WuzfvFyPbLniqGrWLe4dI'
+end
